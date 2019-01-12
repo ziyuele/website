377cf0b68300c507990529427356e05c66d0cb7b
kangjian03 <kangjian03@baidu.com>
2019-01-12 23:35:04 +0800

    add ali reps support

--- src/main/java/com/ziyue/controler/ServerController.java
+++ src/main/java/com/ziyue/controler/ServerController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ServerContoler {
+public class ServerController {
 
     @RequestMapping("/")
     public String test0(){
